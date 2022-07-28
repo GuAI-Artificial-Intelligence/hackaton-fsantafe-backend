736a95fab84dc52a94f0dd1e571e712446a3c42a
bonnett93 <bonnettjordan@gmail.com>
2022-07-28 12:19:03 -0500

    Crear datacollector en emergency

--- modelamiento/app/models/emergency.py
+++ modelamiento/app/models/emergency.py
@@ -3,7 +3,7 @@ from mesa import Model
 from agents.patient import PatientAgent
 
 class EmergencyModel(Model):
-    
+
     def __init__(
         self,
         n,
@@ -14,7 +14,7 @@ class EmergencyModel(Model):
         self.schedule = mesa.time.BaseScheduler(self)
         for i in range(self.num_agents):
             a = PatientAgent(
-                unique_id=i, 
+                unique_id=i,
                 model=self,
                 edad=30,
                 convenio='SURA'
@@ -23,6 +23,15 @@ class EmergencyModel(Model):
 
         # Creación de agentes que cambian el estado del paciente
         self.triage_agent = triage_agent
+        self.datacollector = mesa.DataCollector(
+            model_reporters={"Pacientes": "num_agents"},
+            agent_reporters={"Fase": "fase", "Edad": "edad", "Imagenes": "imagenes",
+                             "Examenes":"examenes", "Interconsulta":"interconsulta",
+                             "Convenio":"convenio","Diagnostico":"diagnostico"}
+        )
+        self.datacollector.collect(self)
+
 
     def step(self):
         self.schedule.step()
+        self.datacollector.collect(self)
